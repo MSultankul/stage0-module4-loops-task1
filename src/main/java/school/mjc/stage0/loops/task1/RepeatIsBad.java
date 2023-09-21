36dace138b0d79601662f04ce1bf270e9657b4e8
Moldashbek Sultankul <sultankulmaksatuly@gmail.com>
2023-09-21 23:24:57 +0600

    sdfdghf

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -1,9 +1,9 @@
 package school.mjc.stage0.loops.task1;
 
 public class RepeatIsBad {
-
-    @SuppressWarnings("java:S106")
     public void repeatIsBad(){
-
+        for(int i=0; i<20; i++){
+            System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
+        }
     }
 }
